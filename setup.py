3bf6dd23169c9851a52d71ede60388fdc11fbca4
Shujia Huang <huangshujia9@gmail.com>
2019-05-21 12:06:28 +0800

    update setup

--- setup.py
+++ setup.py
@@ -18,3 +18,33 @@ URL = 'https:'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = ''
 VERSION = "0.1.0"
+
+if __name__ == "__main__":
+    setup(name=DISTNAME,
+          author=MAINTAINER,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=(open("README.md").read()),
+          license=LICENSE,
+          url=URL,
+          download_url=DOWNLOAD_URL,
+          packages=find_packages(),
+          install_requires=[
+              'pyaml',
+          ],
+          version=VERSION,
+          include_package_data=True,
+          classifiers=[
+             'Intended Audience :: Science/Research',
+             'Programming Language :: Python :: 2.7',
+             'Programming Language :: Python :: 3.4',
+             'License :: OSI Approved :: BSD License',
+             'Topic :: Scientific/Engineering :: Bio-Informatics',
+             'Topic :: Scientific/Engineering :: Tools',
+             'Topic :: Multimedia :: WGS',
+             'Operating System :: POSIX',
+             'Operating System :: Linux/Unix',
+             'Operating System :: MacOS'],
+          )
