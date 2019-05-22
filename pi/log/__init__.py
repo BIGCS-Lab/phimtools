37abbeb8e2836922a4fc54e6ff86583071a2a188
Shujia Huang <huangshujia9@gmail.com>
2019-05-22 08:53:34 +0800

    Olympus -> PI

--- pi/log/__init__.py
+++ pi/log/__init__.py
@@ -1,6 +1,6 @@
 from logbook import Logger
 
-LOG_NAME = "Olympus"
+LOG_NAME = "PI"
 logger = Logger(LOG_NAME)
 logger_cmd = Logger(LOG_NAME + "-commands")
 logger_stdout = Logger(LOG_NAME + "-stdout")
