3bc0c8474fa41d1609b263b3d1c083b2c908cb41
Shujia Huang <huangshujia9@gmail.com>
2019-05-23 23:24:54 +0800

    update the setting information

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ except ImportError:
 
 
 DESCRIPTION = "PI: A python package for phasing and imputation NGS data."
-DISTNAME = 'pi'
-MAINTAINER = 'Shujia Huang'
-MAINTAINER_EMAIL = 'huangshujia9@gmail.com'
-URL = 'https:'
-LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = ''
+DISTNAME = "pi"
+MAINTAINER = "Shujia Huang"
+MAINTAINER_EMAIL = "huangshujia9@gmail.com"
+URL = "https://github.com/ShujiaHuang/pi"
+LICENSE = "BSD (3-clause)"
+DOWNLOAD_URL = ""
 VERSION = "0.1.0"
 
 if __name__ == "__main__":
@@ -32,26 +32,25 @@ if __name__ == "__main__":
           download_url=DOWNLOAD_URL,
           packages=find_packages(),
           install_requires=[
-              'pyaml',
-              'Logbook'
+              "pyaml",
+              "Logbook"
           ],
           version=VERSION,
           include_package_data=True,
           # scripts=[],
           entry_points={
-              'console_scripts': [
-                  'pi = pi.pi_process:main'
+              "console_scripts": [
+                  "pi = pi.pi_process:main"
               ]
           },
           classifiers=[
-             'Intended Audience :: Science/Research',
-             'Programming Language :: Python :: 2.7',
-             'Programming Language :: Python :: 3.4',
-             'License :: OSI Approved :: BSD License',
-             'Topic :: Scientific/Engineering :: Bio-Informatics',
-             'Topic :: Scientific/Engineering :: Tools',
-             'Topic :: Multimedia :: WGS',
-             'Operating System :: POSIX',
-             'Operating System :: Linux/Unix',
-             'Operating System :: MacOS'],
+             "Intended Audience :: Science/Research",
+             "Programming Language :: Python :: 2.7",
+             "Programming Language :: Python :: 3.7",
+             "License :: OSI Approved :: BSD License",
+             "Topic :: Scientific/Engineering :: Bio-Informatics",
+             "Topic :: Scientific/Engineering :: Tools",
+             "Topic :: Multimedia :: WGS",
+             "Operating System :: POSIX",
+             "Operating System :: Linux/Unix"]
           )
