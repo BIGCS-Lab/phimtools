769f7dc3523663bfb71589164e6f52ec0577fbae
Shujia Huang <huangshujia9@gmail.com>
2019-05-26 11:42:40 +0800

    small modify

--- pi/modules/__init__.py
+++ pi/modules/__init__.py
@@ -5,6 +5,7 @@ Date: 2019-05-21
 import sys
 
 from pi.utils import Open
+from pi.log import Log
 
 
 def get_chromlist(input_file):
@@ -18,9 +19,9 @@ def get_chromlist(input_file):
             # first column must be the chromosome id
             chr_id = line.strip().split()[0]
             if chr_id[:3].upper() == "CHR":
-                sys.stderr.write("[ERROR] Target VCF File contains chromosome: %s\n VCF File "
-                                 "can only contain chromosomes 1-22 and X without 'chr' in "
-                                 "the front!!!\n" % chr_id)
+                Log.error("Target VCF File contains chromosome: %s\n VCF File "
+                          "can only contain chromosomes 1-22 and X without 'chr' in "
+                          "the front!!!\n" % chr_id)
                 sys.exit(1)
 
             chr_list.add(chr_id)
