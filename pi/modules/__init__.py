6103ab6ad1cfa107d6ad3cae92be67edfb6040d9
Shujia Huang <huangshujia9@gmail.com>
2019-05-26 15:58:05 +0800

    we do not have check chromosome id has chr or not here

--- pi/modules/__init__.py
+++ pi/modules/__init__.py
@@ -2,10 +2,7 @@
 Author: Shujia Huang
 Date: 2019-05-21
 """
-import sys
-
 from pi.utils import Open
-from pi.log import Log
 
 
 def get_chromlist(input_file):
@@ -18,12 +15,6 @@ def get_chromlist(input_file):
 
             # first column must be the chromosome id
             chr_id = line.strip().split()[0]
-            if chr_id[:3].upper() == "CHR":
-                Log.error("Target VCF File contains chromosome: %s\n VCF File "
-                          "can only contain chromosomes 1-22 and X without 'chr' in "
-                          "the front!!!\n" % chr_id)
-                sys.exit(1)
-
             chr_list.add(chr_id)
 
     return chr_list
