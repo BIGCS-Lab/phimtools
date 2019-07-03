ad5e99753d26b75f379a770805c3e9d636a90fba
Shujia Huang <huangshujia9@gmail.com>
2019-07-03 13:46:18 +0800

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ MAINTAINER_EMAIL = "huangshujia9@gmail.com"
 URL = "https://github.com/ShujiaHuang/pitools"
 LICENSE = "BSD (3-clause)"
 DOWNLOAD_URL = "https://github.com/ShujiaHuang/pitools"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 if __name__ == "__main__":
 
