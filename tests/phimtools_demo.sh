caea1418664514c807929fb908234df42c7f50ee
Shujia Huang <huangshujia9@gmail.com>
2022-01-11 15:40:28 +0800

    更新两个例子

--- tests/phimtools_demo.sh
+++ tests/phimtools_demo.sh
@@ -2,3 +2,5 @@
 phimtools impute -P beagle -M minimac -I data/beagle/test_input.vcf.gz -O z1 -C config.yaml --refpanel-version test_panel --reference-build GRCh38 --nCPU 8
 phimtools impute -P eagle -M minimac -I data/beagle/test_input.vcf.gz -O z2 -C config.yaml --refpanel-version test_panel --reference-build GRCh38 --nCPU 8
 
+#phimtools impute -P beagle -M minimac -I data/beagle/test_input.vcf.gz -O z1 -C config_wcr.yaml --refpanel-version test_panel --reference-build GRCh38 --nCPU 8
+#phimtools impute -P eagle -M minimac -I data/beagle/test_input.vcf.gz -O z2 -C config_wcr.yaml --refpanel-version test_panel --reference-build GRCh38 --nCPU 8
