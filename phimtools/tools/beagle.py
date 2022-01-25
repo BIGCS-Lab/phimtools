2a309bdee01865c069c052e78433f86537f52423
aiyacharley <aiyacharley@outlook.com>
2022-01-25 16:58:31 +0800

    add beagle impute function

--- phimtools/tools/beagle.py
+++ phimtools/tools/beagle.py
@@ -11,7 +11,7 @@ from phimtools.launch import do
 class Beagle(object):
     """A class for Beagle 5.2 program"""
 
-    def __init__(self, config, toolstore, reference_version, chrom):
+    def __init__(self, config, toolstore, reference_version, reference_panel, chrom):
         """basical setting for beagle"""
 
         self.beagle = toolstore["beagle"]
@@ -24,9 +24,16 @@ class Beagle(object):
         if os.path.exists(config["beagle"]["genetic_map_file"][reference_version][chrom]):
             self.genetic_map_file = config["beagle"]["genetic_map_file"][reference_version][chrom]
         else:
-            Log.warn("beagle plink.chr#.GRCh3#.map is missing.\n")
-            Log.warn("Without map=<...> parameter will be performed.\n")
+            Log.warn("beagle plink.chr#.GRCh3#.map is missing.")
+            Log.warn("Without map=<...> parameter will be performed.")
             self.genetic_map_file = None
+
+        if reference_panel:
+            if os.path.exists(config["beagle"]["reference_panel"][reference_panel][chrom]):
+                self.reference_panel = config["beagle"]["reference_panel"][reference_panel][chrom]
+        else:
+            Log.warn("running pre-phased without ref=<...>")
+            self.reference_panel = None
 
     def help(self):
         """Help information for beagle"""
@@ -40,11 +47,22 @@ class Beagle(object):
                 key word parameter for beagle
         """
         if self.genetic_map_file:
-            cmd = " ".join([self.java, '-jar', self.beagle] + 
-                           ["map=%s" % self.genetic_map_file] + 
-                           ["%s=%s" % (k, v) for k, v in kwargs.items()])
+            if self.reference_panel:
+                cmd = " ".join([self.java, '-jar', self.beagle] + 
+                               ["map=%s" % self.genetic_map_file] + 
+                               ["ref=%s" % self.reference_panel] +
+                               ["%s=%s" % (k, v) for k, v in kwargs.items()])
+            else:
+                cmd = " ".join([self.java, '-jar', self.beagle] + 
+                               ["map=%s" % self.genetic_map_file] + 
+                               ["%s=%s" % (k, v) for k, v in kwargs.items()])
         else:
-            cmd = " ".join([self.java, '-jar', self.beagle] + 
-                           ["%s=%s" % (k, v) for k, v in kwargs.items()])
+            if self.reference_panel:
+                cmd = " ".join([self.java, '-jar', self.beagle] + 
+                               ["ref=%s" % self.reference_panel] +
+                               ["%s=%s" % (k, v) for k, v in kwargs.items()])
+            else:
+                cmd = " ".join([self.java, '-jar', self.beagle] + 
+                               ["%s=%s" % (k, v) for k, v in kwargs.items()])
         do.run(cmd)
         return
