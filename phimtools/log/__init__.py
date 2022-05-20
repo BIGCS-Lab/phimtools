5667b35fb78c3126c01316a232367a83b47274d7
aiyacharley <aiyacharley@outlook.com>
2022-05-20 14:40:00 +0800

    update

--- phimtools/log/__init__.py
+++ phimtools/log/__init__.py
@@ -1,9 +1,10 @@
 import sys
-
+import logbook
 from logbook import Logger, StreamHandler
 
 LOG_NAME = "phimtools"
 
+logbook.set_datetime_format("local")
 log_handler = StreamHandler(sys.stderr)
 log_handler.push_application()
 Log = Logger(LOG_NAME)
